c69418c9bf624196dec01300a0852ed923e381ee
TanishkBansode <93660768+TanishkBansode@users.noreply.github.com>
2022-11-16 20:23:55 +0530

    Update workdone.py

--- workdone.py
+++ workdone.py
@@ -7,8 +7,8 @@ def workdone(force, displacement):
 ##################################################
 
 # Eg
-force1 = 5#N
-displacement1 = 2#m
+force1 = 5  # N
+displacement1 = 2  # m
 workdone(force1, displacement1)
 
 #########################################
@@ -16,8 +16,8 @@ workdone(force1, displacement1)
 #########################################
 
 # Eg
-force2 = 5#N
-displacement2 = 0#N coz straight up
+force2 = 5  # N
+displacement2 = 0  # N coz straight up
 
 workdone(force2, displacement2)
 
@@ -26,8 +26,8 @@ workdone(force2, displacement2)
 #########################################
 
 # Eg (when drawing water out of well)
-force3 = -5 #N 
-displacement3 = 2 #m
+force3 = -5  # N
+displacement3 = 2  # m
 
 workdone(force3, displacement3)
 
@@ -35,11 +35,13 @@ workdone(force3, displacement3)
 # When work is against gravity like lifting luggage  #
 ######################################################
 
+
 def workdone1(mass, gravity, displacement):
     return print(f"Workdone = {mass*gravtiy*displacement}")
 
-mass = 15#kg
-displacement4 = 1.5#m
+
+mass = 15  # kg
+displacement4 = 1.5  # m
 
 workdone1(mass=15, gravity=9.8, displacement=displacement4)
 
@@ -48,10 +50,14 @@ workdone1(mass=15, gravity=9.8, displacement=displacement4)
 # When object is in motion like a ball falling from a ground. #
 ###############################################################
 
-def workdone2(mass, final_velocity, initial_velocity):
-	return print(f"Workdone = {0.5 * mass * ((final_velocity ** 2) - (initial_velocity ** 2))}")
 
-mass = 15#kg
-fv = 10#m/s
-fi = 5#m/s
+def workdone2(mass, final_velocity, initial_velocity):
+    return print(
+        f"Workdone = {0.5 * mass * ((final_velocity ** 2) - (initial_velocity ** 2))}"
+    )
+
+
+mass = 15  # kg
+fv = 10  # m/s
+fi = 5  # m/s
 workdone2(mass, fv, fi)
